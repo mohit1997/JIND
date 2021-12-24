5e43aa9c3b14ae7ab4fd3c69a2d427038755f4a8
Mohit Goyal <mohit@idoia-work-01.csl.illinois.edu>
2021-12-24 10:50:30 -0600

    added wrapper

--- extras/seurat_to_pickle.R
+++ extras/seurat_to_pickle.R
@@ -12,12 +12,12 @@ pancreas.list <- SplitObject(panc8, split.by = "tech")
 
 # pancreas.list <- pancreas.list[c("celseq", "celseq2", "fluidigmc1", "smartseq2")]
 source <- pancreas.list$celseq
-source_mat = as.data.frame(t(source@assays$RNA@counts))
+source_mat = as.data.frame(t(as.matrix(source@assays$RNA@counts)))
 cell_labels = source$celltype
 source_mat['labels'] = cell_labels
 
 target <- pancreas.list$celseq2
-target_mat = as.data.frame(t(target@assays$RNA@counts))
+target_mat = as.data.frame(t(as.matrix(target@assays$RNA@counts)))
 cell_labels = target$celltype
 target_mat['labels'] = cell_labels
 
@@ -28,3 +28,8 @@ py_save_object(as.data.frame(source_mat), output_path)
 
 output_path = sprintf("%s/test.pkl", path)
 py_save_object(as.data.frame(target_mat), output_path)
+
+system("source ~/mohit/torch-cpu/bin/activate")
+system("python classify_JIND_rawdata_top.py --train_path /home/mohit/mohit/seurat_to_pkl/train.pkl --test_path /home/mohit/mohit/seurat_to_pkl/test.pkl --column labels")
+
+preds = pd$read_pickle(sprintf("%s/JIND_rawtop_0/JIND_assignmentbrftune.pkl", path))
